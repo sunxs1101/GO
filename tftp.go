2511de31fe9bfccee608df16ac5b03dae6a9480e
sunxingshuai <kpmoving@126.com>
2016-07-29 18:17:09 +0800

    Update tftp.go

--- tftp.go
+++ tftp.go
@@ -6,26 +6,31 @@ import (
 	"bytes"
 	"html/template"
 	"strings"
-
+        
+        "os/exec"
 	"github.com/topicai/candy"
 	"github.com/config/linux_distro.go"
 	"github.com/config/cmd.go"
 	"github.com/wangkuiyi/sh"
 )
 # 判断状态
-if (systemctl status tftpd-hpa | sh.grep(not-found))
-  # 安装
-  if config.LinuxDistro() == "centos"{
+if (config.Cmd("/bin/sh","-c",`systemctl status tftpd-hpa | grep "not-found"`)
+
+if ((exec.Command("/bin/sh","-c",`systemctl status tftpd-hpa | grep "not-found"`) == nil)
+{
+  # 安装tftp
+  linuxdis = config.LinuxDistro()
+  if (linuxdis == "centos"){
     config.Cmd("yum", "install", "tftp-hpa")  
   }
-  else if config.LinuxDistro() == "ubuntu"{
+  else if (linuxdis == "ubuntu"){
     config.Cmd("apt-get", "install", "tftp-hpa")
   }
-  else if config.LinuxDistro() == "coreos"{
-  
+  else if (linuxdis == "coreos"){
+    config.Cmd("docker","pull","jumanjiman/tftp-hpa")
   }
-
-else if(systemctl status tftpd-hpa | sh.grep(inactive))
-    service tftpd-hpa restart
+}
+else if((exec.Command("/bin/sh","-c", `systemctl status tftpd-hpa | sh.grep "inactive") == nil)
+    config.Cmd("service","tftpd-hpa","restart")
 
 
