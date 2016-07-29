3ac3a368b60e43e17092686410aaf2ffcd2847c3
sunxingshuai <kpmoving@126.com>
2016-07-29 23:31:49 +0800

    Update tftp.go

--- tftp.go
+++ tftp.go
@@ -1,30 +1,68 @@
 package config
 
 import (
-	"bytes"
-	"html/template"
-	"strings"
         "os/exec"
-	"github.com/topicai/candy"
-	"github.com/config"
-	"github.com/wangkuiyi/sh"
+	"github.com/k8sp/auto-install/config"
 )
 
-//if ((exec.Command("/bin/sh","-c",`systemctl status tftpd-hpa | grep "not-found"`) == nil)
-if ((config.Cmd("/bin/sh","-c",`systemctl status tftpd-hpa | grep "not-found"`) == nil)
+linuxdis := config.LinuxDistro()   
+if _,e := exec.Command("/bin/sh","-c",`systemctl status tftpd-hpa | grep "not-found"`).StdoutPipe(); e == nil; linuxdis == "ubuntu"
+{
+	config.Cmd("apt-get", "install", "tftp-hpa")
+}
+else if _,e := exec.Command("/bin/sh","-c",`systemctl status tftp | grep "not-found"`).StdoutPipe(); e == nil; linuxdis == "centos"
+{                 
+    config.Cmd("yum", "install", "tftp-server")  
+}
+else if linuxdis == "coreos"
+{
+    config.Cmd("docker","run","jumanjiman/tftp-hpa")
+}
+
+if _,e := exec.Command("/bin/sh","-c",`systemctl status tftpd-hpa | grep "inactive"`).StdoutPipe(); e == nil; linuxdis == "ubuntu"
+{
+	config.Cmd("service","tftpd-hpa","restart")
+}
+else if _,e := exec.Command("/bin/sh","-c",`systemctl status tftp | grep "inactive"`).StdoutPipe(); e == nil; linuxdis == "centos"
+{
+	config.Cmd("service","tftp","restart")
+}
+
+
+
+if _,e := exec.Command("/bin/sh","-c",`systemctl status tftpd-hpa | grep "not-found"`).StdoutPipe(); e == nil
 {
   linuxdis = config.LinuxDistro()
-  if (linuxdis == "centos"){
+  if linuxdis == "centos"{
     config.Cmd("yum", "install", "tftp-server")  
   }
-  else if (linuxdis == "ubuntu"){
+  else if linuxdis == "ubuntu"{
     config.Cmd("apt-get", "install", "tftp-hpa")
   }
-  else if (linuxdis == "coreos"){
+  else if linuxdis == "coreos"{
     config.Cmd("docker","run","jumanjiman/tftp-hpa")
   }
 }
-else if((config.Cmd("/bin/sh","-c", `systemctl status tftpd-hpa | sh.grep "inactive") == nil)
+
+
+//if ((exec.Command("/bin/sh","-c",`systemctl status tftpd-hpa | grep "not-found"`) == nil)
+if ((config.Cmd("/bin/sh","-c",`systemctl status tftpd-hpa | grep "not-found"`) == nil)||
+	(config.Cmd("/bin/sh","-c",`systemctl status tftp | grep "not-found"`) == nil))
+
+{
+  linuxdis = config.LinuxDistro()
+  if linuxdis == "centos"{
+    config.Cmd("yum", "install", "tftp-server")  
+  }
+  else if linuxdis == "ubuntu"{
+    config.Cmd("apt-get", "install", "tftp-hpa")
+  }
+  else if linuxdis == "coreos"{
+    config.Cmd("docker","run","jumanjiman/tftp-hpa")
+  }
+}
+else if((config.Cmd("/bin/sh","-c",`systemctl status tftpd-hpa | grep "inactive"`) == nil)||
+	(config.Cmd("/bin/sh","-c",`systemctl status tftp | grep "inactive"`) == nil))
 {
     config.Cmd("service","tftpd-hpa","restart")
 }
